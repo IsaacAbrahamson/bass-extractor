adf998155412f4482742f1b7b9ceb3ea83670c23
Isaac Abrahamson <isaac.abrahamson@programmer.net>
2022-09-01 21:53:10 -0400

    pseudocode

---
+++ app.js
@@ -0,0 +1,23 @@
+// Input youtube song
+// https://www.youtube.com/watch?v=Gs069dndIYk
+
+// Identify song
+
+// Get lyrics
+
+// Get cover art
+
+// Separate stems
+// https://github.com/deezer/spleeter
+
+// Extract notes, cords, key
+// http://www.sonicapi.com/
+
+// Convert notes to sheet music
+
+// Display notes on screen
+
+// visualization
+// https://webaudioapi.com/samples/visualizer/
+// https://gg-gina.medium.com/how-to-music-visualizer-web-audio-api-aa007f4ea525
+
